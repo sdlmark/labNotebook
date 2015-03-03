de7fc5d2de2f1ab2e426c4eb4a85ea664f70cabd
Stephanie Mark <stephaniemark@gmail.com>
2015-03-03 13:26:56 -0500

    with new nostranded data pt

--- countNotesData.R
+++ countNotesData.R
@@ -10,11 +10,14 @@ colnames(rawdata) = c("sample", "type", "feature", "stranded", "mode", "gff", "g
 prop_counted = (rawdata$total_reads - (rawdata$no_feature+rawdata$ambiguous+rawdata$not_unique))/rawdata$total_reads 
 
 prop_expr = rawdata$nonzero/rawdata$genes
+prop_ambig = rawdata$ambiguous/rawdata$total_reads
+prop_multimap = rawdata$not_unique/rawdata$total_reads
 
-paramdata = cbind(rawdata, prop_counted, prop_expr)
-head(paramdata)
+paramdata = cbind(rawdata, prop_counted, prop_expr, prop_ambig, prop_multimap)
+paramdata
 
 write.csv(as.data.frame(paramdata), "~/labNotebook/htseqprelimdata.csv")
+write.table(as.data.frame(paramdata), "~/labNotebook/htseqprelimdata.txt")
 
 #totalcounts = rawdata$nonzero + rawdata$no_feature + rawdata$ambiguous + rawdata$not_unique
 #totalcounts
