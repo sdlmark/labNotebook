8a48422c107140c8a6e1fc1a9b9fbe4075854481
Stephanie Mark <stephaniemark@gmail.com>
2015-03-03 12:07:48 -0500

    prelim data txt format

--- countNotesData.R
+++ countNotesData.R
@@ -7,11 +7,18 @@ rawdata = as.data.frame(read.table("countNotes.txt"))
 
 colnames(rawdata) = c("sample", "type", "feature", "stranded", "mode", "gff", "genes", "nonzero", "no_feature", "ambiguous", "not_unique", "total_reads")
 
-# paramdata()
+prop_counted = (rawdata$total_reads - (rawdata$no_feature+rawdata$ambiguous+rawdata$not_unique))/rawdata$total_reads 
 
-prop_counted = (rawdata$total_reads - rawdata$no_feature)/rawdata$total_reads 
+prop_expr = rawdata$nonzero/rawdata$genes
 
-paramdata = cbind(rawdata, prop_counted)
-paramdata
+paramdata = cbind(rawdata, prop_counted, prop_expr)
+head(paramdata)
 
+write.csv(as.data.frame(paramdata), "~/labNotebook/htseqprelimdata.csv")
+
+#totalcounts = rawdata$nonzero + rawdata$no_feature + rawdata$ambiguous + rawdata$not_unique
+#totalcounts
+#rawdata$total_reads
+
+#count_compare = data.frame(totalcounts, rawdata$total_reads)
 
